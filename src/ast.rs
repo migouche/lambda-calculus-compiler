532ceefe7613e02a77be2fbc8d5b103ea9a5626d
migouche <46398743+migouche@users.noreply.github.com>
2024-10-23 00:49:03 +0200

    finished ast basic structure? (gotta work out AST)

--- src/ast.rs
+++ src/ast.rs
@@ -2,11 +2,36 @@ use core::str;
 use std::rc::Rc;
 
 enum Value {
-    Eval(Rc<LamdaFunction>, Rc<Value>),
-    AnonFunc(String, Rc<LamdaFunction>)
+    Eval(Rc<Variable>, Rc<Value>),
+    AnonFunc(LamdaFunction),
+    Arg(String)
+}
+
+struct Variable{
+    name: String,
+    val: Value
 }
 
 struct LamdaFunction{
     argname: String,
     value: Rc<Value>
 }
+
+struct AST{
+    pub program: Vec<Variable>
+}
+
+/*
+true = lambda x. lambda y. x;
+
+Variable(
+    "true",
+    Value::AnonFunc(
+        "x",
+        Value::AnonFunc(
+            "y",
+            Value::Arg("x")
+        ))
+    )
+
+*/
